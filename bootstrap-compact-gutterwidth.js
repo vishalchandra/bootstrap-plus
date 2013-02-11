fff3d88f664982a3002a2ad3ff83ad39f41dc415
Vishal Chandra <vishalchandra@gmail.com>
2013-02-10 18:27:22 -0800

    Updated to cater for scenario in which initial screen is phone, which caused incorrect calculation of gutter and column widths

--- bootstrap-compact-gutterwidth.js
+++ bootstrap-compact-gutterwidth.js
@@ -2,26 +2,40 @@
   works with tablet and desktop mode. To make row with compact 
   gutter width add a new class compact to div along with class 
   row-fluid and also add class .hidden-phone since only works 
-  for tablet and desktop mode.                             */
+  for tablet and desktop screen mode.                         */
 
- $(document).ready(function(){
-    $(".row-fluid.compact").each(function(){
-        var tmp_numl=0; //No. of columns in the row
-        var tmp_ml_total=0; //Sum of left margins
-        var tmp_cf=0.3; //Compact Factor 0<tmp_cf<=1
-        var tmp_norm=100/parseFloat($(this).css("width")); //Normalize to percentage for fluidity
+$(document).ready(function(){
+    init_screen_phone = $(".row-fluid.compact").is(':hidden') ? 1 : 0; //1 if phone
 
-        $(this).children("div[class^='span']").each(function(){
-            tmp_numl++;
-            tmp_ml_total+=parseFloat($(this).css("marginLeft"));
+    makecompact = function(){ // Compact function should be called only once
+        $(".row-fluid.compact").each(function(){
+            var tmp_numl=0; //No. of columns in the row
+            var tmp_ml_total=0; //Sum of left margins
+            var tmp_cf=0.3; //Compact Factor 0<tmp_cf<=1
+            var tmp_norm=100/parseFloat($(this).css("width")); //Normalize to percentage for fluidity
+
+            $(this).children("div[class^='span']").each(function(){
+                tmp_numl++;
+                tmp_ml_total+=parseFloat($(this).css("marginLeft"));
+            });
+
+            $(this).children("div[class^='span']").each(function(){
+                var tmp_w=parseFloat($(this).css("width"));
+                var tmp_ml=parseFloat($(this).css("marginLeft"));
+                $(this).css("marginLeft",(tmp_ml*tmp_cf*tmp_norm)+"%");
+                $(this).css("width",tmp_norm*(tmp_w+(((1-tmp_cf)*tmp_ml_total)/tmp_numl))+"%");
+            });
         });
+    }
 
-        $(this).children("div[class^='span']").each(function(){
-            var tmp_w=parseFloat($(this).css("width"));
-            var tmp_ml=parseFloat($(this).css("marginLeft"));
-            $(this).css("marginLeft",(tmp_ml*tmp_cf*tmp_norm)+"%");
-            $(this).css("width",tmp_norm*(tmp_w+(((1-tmp_cf)*tmp_ml_total)/tmp_numl))+"%");
-        });
+    $(window).resize(function(){ // Check for screen change
+        if($(".row-fluid.compact").is(':visible') && init_screen_phone){
+            init_screen_phone = 0; // Disables next calling of compact function 
+            makecompact(); // Apply compact function
+        }
     });
+
+    if(!init_screen_phone)
+        makecompact(); // If initially screen not phone, apply compact function
 });
     
